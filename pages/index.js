d5743604d49c8faaf9aa68beb2f4fe1ad1534e9c
Hector Flores <hecto932@gmail.com>
2019-04-20 12:44:15 -0500

    Creating our first page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <p>Bienvenidos al curso de Next.js</p>
+      </div>
+    );
+  }
+}
