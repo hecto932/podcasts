f5d7be1e1a6e691e9baad7258289d208bd7d5141
Hector Flores <hecto932@gmail.com>
2019-04-20 13:20:33 -0500

    Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,29 @@ export default class extends React.Component {
       <div>
         <h1>Hello world!</h1>
         <p>Bienvenidos al curso de Next.js</p>
+
+        <img src="/static/platzi-logo.png" alt="plazi"/>
+
+        <style jsx>{`
+          h1 {
+            color: red;
+          }
+          div :global(p) {
+            color: green;
+          }
+          img {
+            max-width: 50%;
+            display: block;
+            margin: 0 auto;
+          }
+        `}
+        </style>
+
+        <style jsx global>{`
+          body {
+            background: white;
+          }
+        `}</style>
       </div>
     );
   }
